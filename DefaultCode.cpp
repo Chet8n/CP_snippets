84e1b0108f51c2557c98e2f4b44d67867c2b1105
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-07-15 12:26:30 +0530

    Create DefaultCode.cpp

---
+++ DefaultCode.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const ll mod = 1e9 + 7;
+const ll inf = 2e9 + 5;
+double PI = 3.14159265358979323846;
+
+
+void solve() {
+
+    
+}
+
+int32_t main()
+{
+    IOS
+    
+    int T;
+    T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+}
