a38db169dd9513e38a504aca22a6689a72218745
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-07-27 09:44:40 +0530

    Create MultiplicativeInverse.cpp

---
+++ MultiplicativeInverse.cpp
@@ -0,0 +1,19 @@
+template <typename T>
+T inverse(T a, T m) {
+	a = a % m;
+	if (a < 0) {
+		a += m;
+	}
+	T b = m, u = 0, v = 1;
+	while (a != 0) {
+		T t = b / a;
+		b -= a * t;
+		swap(a, b);
+		u -= v * t;
+		swap(u, v);
+	}
+	if (u < 0) {
+		u += m;
+	}
+	return u;
+}
