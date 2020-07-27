028acc9808491c6f724650536cadd439ca110c97
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-07-27 12:46:08 +0530

    Update ModularMultiplication.cpp

--- ModularMultiplication.cpp
+++ ModularMultiplication.cpp
@@ -1,9 +1,9 @@
-template<typename T> T mMULTIPLY(T a, T b, T m) {
-	T res = 0;
+ll mMULTIPLY(ll a, ll b, ll m) {
+	ll res = 0;
 	while (b) {
 		if (b & 1ll)
 			res = (res + a) % m;
-		b >> 1;
+		b = b >> 1;
 		a = (a * 2) % m;
 	}
 	return res % m;
