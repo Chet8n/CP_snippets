0768b8dbb18fae8ab9e1e0965a7986ece69fe8fa
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-07-27 12:46:49 +0530

    Update FastModuloPower.cpp

--- FastModuloPower.cpp
+++ FastModuloPower.cpp
@@ -1,10 +1,10 @@
-template<typename T> T mPOW(T a, T b, ll m) {
-	T ans = 1;
+ll mPOW(ll a, ll b, ll m) {
+	ll ans = 1;
 	while (b) {
 		if (b & 1ll)
-			ans = (ans * a) % m;
-		b >> 1;
-		a = (a * a) % m;
+			ans = mMULTIPLY(ans, a, m);
+		b = b >> 1;
+		a = mMULTIPLY(a, a, m);
 	}
 	return ans % m;
 }
