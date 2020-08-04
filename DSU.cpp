27be1d2d0f69c14553953e84b0d8eba675baf431
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-08-04 12:04:44 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,36 +1,18 @@
 struct dsu {
 
-    vector<int> par, sz;
-    int total_component;
-
-    //initialize:
-    void init(int n) {
-        par.resize(n);
-        sz.resize(n);
-        for (int i = 0; i < n; i++) {
-            par[i] = i;
-        }
-        for (int i = 0; i < n; i++) {
-            sz[i] = 1;
-        }
-        total_component = n;
+    vector<int> p;
+    void init(int NN) {
+        p.clear(); p.resize(NN);
+        iota(p.begin(), p.end(), 0)
     }
-
-    int get_superparent(int x) {
-        if (x == par[x]) {
-            return x;
-        }
-        return par[x] = get_superparent(par[x]);
+    int get(int x) {
+        return (x == p[x] ? x : (p[x] = get(p[x])));
     }
-
-    void unite(int x, int y) {
-        int spx = get_superparent(x);
-        int spy = get_superparent(y);
-        if (spx != spy) {
-            par[spx] = spy;
-            sz[spy] = sz[spy] + sz[spx];
-            sz[spx] = 0;
+    void unite(int x, int y){
+        x = get(x);
+        y = get(y);
+        if(x!=y){
+            p[x] = y;
         }
-        total_component--;
-    }
+    }   
 };
