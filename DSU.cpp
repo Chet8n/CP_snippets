07454ce29150a84998abb7f8bddc66766487549c
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-08-04 16:21:07 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -3,7 +3,7 @@ struct dsu {
     vector<int> p;
     void init(int NN) {
         p.clear(); p.resize(NN);
-        iota(p.begin(), p.end(), 0)
+        iota(p.begin(), p.end(), 0);
     }
     int get(int x) {
         return (x == p[x] ? x : (p[x] = get(p[x])));
