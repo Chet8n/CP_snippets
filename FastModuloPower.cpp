7cb2dd2ffd5d916ba1e05307fbb8f4e32fd8e39c
Chet8n <64730540+Chet8n@users.noreply.github.com>
2020-08-19 10:50:03 +0530

    Update FastModuloPower.cpp

--- FastModuloPower.cpp
+++ FastModuloPower.cpp
@@ -9,6 +9,7 @@ ll mMULTIPLY(ll a, ll b, ll m) {
 	return res % m;
 }
 
+// for multiplying 10^18 * 10^18(ll * ll)
 ll mPOW(ll a, ll b, ll m) {
 	ll ans = 1;
 	while (b) {
@@ -18,4 +19,19 @@ ll mPOW(ll a, ll b, ll m) {
 		a = mMULTIPLY(a, a, m);
 	}
 	return ans % m;
+}
+
+// for multiplying 10^9 * 10^9(int * int)
+ll mpow(ll a, ll b, ll m = mod) {
+    ll ans = 1;
+    while (b) {
+        if (b & 1ll) {
+            ans *= a;
+            ans %= m;
+        }
+        a *= a;
+        a %= m;
+        b = b >> 1;
+    }
+    return ans;
 }
